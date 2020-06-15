c398edb85e702714379b2475e5e4b653498c1c64
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-15 19:06:24 +0800

    Adding Pagination to the Spider

--- dc/data_checker/spiders/dataset.py
+++ dc/data_checker/spiders/dataset.py
@@ -6,6 +6,7 @@ class DatasetSpider(scrapy.Spider):
     name = 'dataset'
     allowed_domains = ['catalog.data.gov']
     start_urls = ['http://catalog.data.gov/dataset/']
+    max_pages = 5
 
     def parse(self, response):
         host = response.url.split("/dataset")[0]
@@ -15,3 +16,9 @@ class DatasetSpider(scrapy.Spider):
                 link=host + dataset.css("h3.dataset-heading > a::attr(href)").get(),
                 organization=dataset.css(".dataset-organization::text").get().strip(" —")
             )
+
+        for link in response.css(".pagination > ul > li:last-child:not(.active) > a"):
+            page_number = int(link.attrib['href'].split('=')[1])
+            if page_number > self.max_pages:
+                break
+            yield response.follow(link, callback=self.parse)
