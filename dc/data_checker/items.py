2faa63fc1a23b0c356566fb4ce191689641262b7
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-15 19:04:17 +0800

    Creating a Spider

--- dc/data_checker/items.py
+++ dc/data_checker/items.py
@@ -11,4 +11,6 @@ import scrapy
 class DataCheckerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    link = scrapy.Field()
+    organization = scrapy.Field()
